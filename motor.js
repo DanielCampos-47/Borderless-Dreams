df5805fb27ccc85be9c30e8d431e11078441a185
DanielCampos-47 <81194412+DanielCampos-47@users.noreply.github.com>
2023-03-06 11:09:28 -0500

    Add files via upload

--- motor.js
+++ motor.js
@@ -1,4 +1,16 @@
 
+// Dropdown menu (getElementByClassName)
+function toggleNav (elementName) {
+    let x = document.getElementById(elementName);
+    if (x.style.display === "" || x.style.display === "none") {
+        x.style.display = "flex"
+    } else {
+        x.style.display = "none"
+    }
+}
+
+
+//Financial: bubble-text display toggle
 function toggleDisplay (elementName) {
     let x = document.getElementById(elementName);
     if (x.style.display === "block" || x.style.display === "") {
